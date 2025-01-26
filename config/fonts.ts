9d2e854f871b65d51458ab4e9f82031ab99e55d7
shirafzx <shirafzx@shirafzx.local>
2025-01-26 12:25:52 +0700

    :lipstick: optimized font - add subsets

--- config/fonts.ts
+++ config/fonts.ts
@@ -2,15 +2,18 @@ import { Inter, Noto_Sans_Thai, Noto_Serif_JP } from "next/font/google";
 
 export const inter = Inter({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
   variable: "--font-inter",
 });
 
 export const notoSansThai = Noto_Sans_Thai({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["thai"],
   variable: "--font-notoSerifThai",
 });
 
 export const notoSerifJP = Noto_Serif_JP({
   weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
   variable: "--font-notoSerifJP",
 });
